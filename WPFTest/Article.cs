d101ad4283ad5eebb8a08d367b4cd317504881de
asd6224868@hotmail.com <asd6224868@hotmail.com>
2015-01-13 23:01:36 +0800

    test regex, listview

---
+++ WPFTest/Article.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace WPFTest
+{
+    class Article
+    {
+        private Article()
+        {
+
+        }
+        /// <summary>
+        /// 根据URL返回摘要
+        /// </summary>
+        /// <param name="url"></param>
+        /// <returns></returns>
+        public static IList<Article> CreateArticles(string url)
+        {
+            var result = client.GetStringAsync(new Uri(url, UriKind.Absolute)).Result;
+            var matchs = regexShot.Matches(result);
+            List<Article> list = new List<Article>();
+            foreach(Match m in matchs)
+            {
+                var str = Regex.Replace(m.Value, "</p>", "\n");
+                str = Regex.Replace(str, "</?[^>]+/?>","");
+                str = Regex.Replace(str, "&nbsp;", " ");
+                list.Add(new Article() { Content =str});
+            }
+            return list;
+        }
+        public string Author { get; set; }
+        public string Hot { get; set; }
+        public DateTime PublishTime { get; set; }
+        public string Head { get; set; }
+        public string ShortContent { get; set; }
+        public string Url { get; set; }
+        public string Content { get; set; }
+
+        private static HttpClient client = new HttpClient();
+        private static string shortContent = @"<div class=""short"">[\s\S]*?<div class=""shot"">[\s\S]*?</div>[\s\S]*?</div>";
+        private static Regex regex = new Regex(shortContent);
+        private static string shotContent = "<div class=\"shot\">[\\s\\S]*?</div>";
+        private static Regex regexShot = new Regex(shotContent);
+    }
+}
---
+++ WPFTest/MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPFTest
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            var arts= Article.CreateArticles("http://meiwenrishang.com/");
+            list.ItemsSource = arts;
+        }
+
+        private void list_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            MessageBox.Show("fuck");
+        }
+
+    }
+}
