3887ddab57bb7d627c146373f1d8c50a98c9d09c
asd6224868@hotmail.com <asd6224868@hotmail.com>
2015-01-19 22:08:33 +0800

    ListViewTest

--- WPFTest/Article.cs
+++ WPFTest/Article.cs
@@ -23,18 +23,23 @@ namespace WPFTest
         {
             var result = client.GetStringAsync(new Uri(url, UriKind.Absolute)).Result;
             var matchs = Regex.Matches(result, shotContentReg);
+            var headMatchs = Regex.Matches(result, headReg);
+            var timeMatchs = Regex.Matches(result, timeTagReg);
             List<Article> list = new List<Article>();
-            foreach(Match m in matchs)
+            for (int i = 0; i < matchs.Count; i++)
             {
+                if (i == 0) continue; //过滤掉对应的广告
+                Match m = matchs[i];
                 string hrefs = Regex.Match(m.Value, hrefReg).Value;
                 var realHrefs = Regex.Match(hrefs, realHrefReg).Value;
                 var str = Regex.Replace(m.Value, "</p>", "\n");
-                str = Regex.Replace(str, "</?[^>]+/?>","");
+                str = Regex.Replace(str, "</?[^>]+/?>", "");
                 str = Regex.Replace(str, "&nbsp;", " ");
-
-                var heads = Regex.Match(result, headReg).Value;
-                heads = Regex.Replace(heads, "</?[^>]+/?>", "");
-                list.Add(new Article() { Content = str, Url = realHrefs, Head = heads });
+                str = Regex.Replace(str, "继续阅读", " ");
+                str = str.TrimStart(new char[] { ' ', '\n' }).TrimEnd(new char[] { ' ', '\n' });
+                var heads = Regex.Replace(headMatchs[i].Value, "</?[^>]+/?>", "");
+                var time = Regex.Replace(timeMatchs[i].Value, "</?[^>]+/?>", "");
+                list.Add(new Article() { Content = str, Url = realHrefs, Head = heads, Time = time });
             }
             return list;
         }
@@ -61,13 +66,15 @@ namespace WPFTest
         public string Url { get; set; }
         public string Content { get; set; }
         public string ImageUrl { get; set; }
+        public string Time { get; set; }
         private static HttpClient client = new HttpClient();
         private static string shortContentReg = "<div class=\"short\">[\\s\\S]*?<div class=\"shot\">[\\s\\S]*?</div>[\\s\\S]*?</div>";
         private static string shotContentReg = "<div class=\"shot\">[\\s\\S]*?</div>";
         private static string hrefReg = "<a href=\"http://meiwenrishang.com/post/(.*?) class=\"cont\">";
         private static string realHrefReg = @"http://meiwenrishang.com/post/[\w-/]*";
         private static string headReg = "<h2>.*</h2>";
-        private static string imageReg = "http://.*?jpeg";
+        private static string imageReg = "http://([\\s\\S]*?)jpeg";
         private static string contentReg = "<div class=\"shot\">[\\s\\S]*?<p>——————————————————————————</p>";
+        private static string timeTagReg = "<strong id=\"timebg\">.*?</strong>";
     }
 }
